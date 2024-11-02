8ff5990d2e6cb822bac281e8be1fdf521376af5e
Cuong Nguyen Van <cuong.nv281203@gmail.com>
2024-11-02 15:34:26 +0700

    Initial commit: Project setup and Dashboard Wrapper

---
+++ src/app/dashboardWrapper.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const dashboardWrapper = ({ children } : { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+        {/* Sidebar */}
+        <main 
+            className={`flex w-full flex-col bg-gray-50 dark:bg-darg-bg md:pl-64`}
+        >
+            {/* Navbar */}
+            {children}
+        </main>
+    </div>
+  );
+};
+
+export default dashboardWrapper;
