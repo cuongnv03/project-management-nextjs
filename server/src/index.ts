6abb046ca7091479c060dae6270a2e1362947b98
Cuong Nguyen Van <cuong.nv281203@gmail.com>
2024-11-08 01:08:43 +0700

    controllers and routes for project and task model

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import { ajax } from './../node_modules/rxjs/src/internal/ajax/ajax';
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+/* import routes */
+import projectRoutes from './routes/projectRoutes';
+
+/* configure */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* routes */
+app.get('/', (req, res) => {
+  res.send('This is home route');
+});
+
+app.use('/projects', projectRoutes);
+
+/* server */
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
