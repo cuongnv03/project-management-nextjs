a3eed5d95b34612ea0bc349656a8e4ab656c9170
Cuong Nguyen Van <cuong.nv281203@gmail.com>
2024-11-21 14:05:47 +0700

    basic completion

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 
 export interface Project {
   id: number;
@@ -60,11 +61,52 @@ export interface Task {
   attachments?: Attachment[];
 }
 
+export interface SearchResults {
+  tasks?: Task[];
+  projects?: Project[];
+  users?: User[];
+}
+
+export interface Team {
+  teamId: number;
+  teamName: string;
+  productOwnerUserId?: number;
+  projectManagerUserId?: number;
+}
+
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+    // prepareHeaders: async (headers) => {
+    //   const session = await fetchAuthSession();
+    //   const { accessToken } = session.tokens ?? {};
+    //   if (accessToken) {
+    //     headers.set("Authorization", `Bearer ${accessToken}`);
+    //   }
+    //   return headers;
+    // },
+  }),
   reducerPath: "api",
-  tagTypes: ["Projects", "Tasks"],
+  tagTypes: ["Projects", "Tasks", "Users", "Teams"],
   endpoints: (build) => ({
+    // getAuthUser: build.query({
+    //   queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
+    //     try {
+    //     //   const user = await getCurrentUser();
+    //     //   const session = await fetchAuthSession();
+    //     //   if (!session) throw new Error("No session found");
+    //     //   const { userSub } = session;
+    //     //   const { accessToken } = session.tokens ?? {};
+
+    //       const userDetailsResponse = await fetchWithBQ(`users/${userSub}`);
+    //       const userDetails = userDetailsResponse.data as User;
+
+    //       return { data: { user, userSub, userDetails } };
+    //     } catch (error: any) {
+    //       return { error: error.message || "Could not fetch user data" };
+    //     }
+    //   },
+    // }),
     getProjects: build.query<Project[], void>({
       query: () => "projects",
       providesTags: ["Projects"],
@@ -109,6 +151,17 @@ export const api = createApi({
         { type: "Tasks", id: taskId },
       ],
     }),
+    getUsers: build.query<User[], void>({
+      query: () => "users",
+      providesTags: ["Users"],
+    }),
+    getTeams: build.query<Team[], void>({
+      query: () => "teams",
+      providesTags: ["Teams"],
+    }),
+    search: build.query<SearchResults, string>({
+      query: (query) => `search?query=${query}`,
+    }),
   }),
 });
 
@@ -118,4 +171,9 @@ export const {
   useGetTasksQuery,
   useCreateTaskMutation,
   useUpdateTaskStatusMutation,
+  useSearchQuery,
+  useGetUsersQuery,
+  useGetTeamsQuery,
+  useGetTasksByUserQuery,
+  //   useGetAuthUserQuery,
 } = api;
