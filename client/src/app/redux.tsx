f311e628c064f5f552c33b468e1d874b81898109
Cuong Nguyen Van <cuong.nv281203@gmail.com>
2024-11-29 01:45:32 +0700

    modify

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -26,13 +26,13 @@ import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 /* REDUX PERSISTENCE */
 const createNoopStorage = () => {
   return {
-    getItem() {
+    getItem(_key: any) {
       return Promise.resolve(null);
     },
     setItem(_key: any, value: any) {
       return Promise.resolve(value);
     },
-    removeItem() {
+    removeItem(_key: any) {
       return Promise.resolve();
     },
   };
